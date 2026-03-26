ad518639e8432a16d43833a0dca2af3858afcf08
brabvs77-eng <brabvs77@gmail.com>
2026-03-26 23:05:32 +0200

    Updated/Added simply-static-1-1774557040

---
+++ assets/vendor/wp-yandex-metrika/assets/popupMaker.js
@@ -0,0 +1,2 @@
+jQuery(function($){if(window.wpym===undefined){return;}
+const{counters}=window.wpym;$(this).on('pumAfterOpen',function(e,data){if(!!$(e.target).find('form').length){counters.forEach(counter=>{ym(counter.number,'reachGoal','ym-open-leadform');});}});});
