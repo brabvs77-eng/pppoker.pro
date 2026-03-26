fe2dc970e1280c2231071f635accf07263707b16
brabvs77-eng <brabvs77@gmail.com>
2026-03-26 22:40:18 +0200

    Updated/Added simply-static-1-1774557040

---
+++ assets/vendor/elementor/eslint-local-rules.js
@@ -0,0 +1,1 @@
+const elementorEditorPlugin=require('./packages/packages/tools/eslint-plugin-editor/dist/index.js');module.exports={'no-react-namespace':elementorEditorPlugin.rules['no-react-namespace'],};
